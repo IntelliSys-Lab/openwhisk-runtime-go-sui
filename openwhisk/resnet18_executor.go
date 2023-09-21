476171aec55b591fd9c5969c46a9a51cd72897a3
Wisley1998 <suiyifan@sjtu.edu.cn>
2023-09-21 11:44:15 +0800

    second commit

--- openwhisk/resnet18_executor.go
+++ openwhisk/resnet18_executor.go
@@ -77,14 +77,13 @@ func (proc *resnet18Executor) Start(waitForAck bool) error {
 	//reader, writer := io.Pipe()
 	//proc.cmd.Stdout = io.MultiWriter(os.Stdout, writer)
 	//proc.output = bufio.NewReader(reader)
+	proc.started = true
 
 	err := proc.cmd.Start()
 	if err != nil {
 		proc.cmd = nil // No need to keep the command around if it failed to start
 		return fmt.Errorf("failed to start command: %w", err)
 	}
-
-	proc.started = true
 
 	Debug("Executor Finished pre-loading ResNet18.")
 
